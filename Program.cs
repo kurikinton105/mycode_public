c6cdfb09f10acf7b0ba79638f75a971af910c57e
kurikinton105 <yamadakenta0101@gmail.com>
2020-04-14 13:21:19 +0900

    データ抽出を実行(json)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
@@ -21,20 +22,50 @@ namespace Questions251616
             var result = await obj.LoginAsync(user, password);
             if (result == "[{\"id\":1401,\"code\":\"USER_NOT_FOUND_ERROR\",\"message\":\"The user identified by  and the password is not found.\"}]")
             {
-            Console.WriteLine("Wrong the ID or Password!!");
+                Console.WriteLine("Wrong the ID or Password!!");
             }
-            else{
+            else
+            {
                 Console.WriteLine(result);
                 Console.WriteLine();
             }
-            
+            /***
+            //問題取得用
+            string[] ABC = new string[4] { "A", "B", "C", "D" };
+            for (int i = 1; i <= 11; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    string problemID = "ITP1_" + i + "_" + ABC[j];
+                    // 文字コードを指定
+                    Encoding enc = Encoding.GetEncoding("UTF-16");
+                    for (int k = 0; k < 20; k++)
+                    {
+                        var IDA = k + 1;
+                        string code = "testcases/" + problemID + "/" + IDA;
+                        //Console.WriteLine(code);
+                        var problem = await obj.GetproblemAsync(code);
+                        //Console.WriteLine(problem);
 
+                        // ファイルを開く
+                        StreamWriter writer = new StreamWriter(problemID + "_" + IDA + ".json", false, enc);
+                        // テキストを書き込む
+                        writer.WriteLine(problem);
+                        Console.WriteLine(problemID + "/" + IDA);
+                        // ファイルを閉じる
+                        writer.Close();  
+                    }    
+                }
+                
+            }
+            ***/
             var self = await obj.SelfAsync();
             Console.WriteLine(self);
-            var submission = await obj.SubmissionAsync("ITP1_1_A","Python", "print('Hello World')");
+            var submission = await obj.SubmissionAsync("ITP1_1_A", "Python", "print('Hello World')");
             Console.WriteLine(submission);
             var Judge = await obj.JudgeAsync((4252705));
             Console.WriteLine(Judge);
+            
             Console.ReadKey();
         }
     }
@@ -42,9 +73,10 @@ namespace Questions251616
     public class API
     {
         //プログラム作成参考( https://teratail.com/questions/251616)
-        private static readonly HttpClient client = new HttpClient(new HttpClientHandler() )//{ UseCookies = true, })
+        private static readonly HttpClient client = new HttpClient(new HttpClientHandler())//{ UseCookies = true, })
         {
-            BaseAddress = new Uri("https://judgeapi.u-aizu.ac.jp/"),
+            BaseAddress = new Uri("https://judgeapi.u-aizu.ac.jp"),
+            //BaseAddress = new Uri("https://judgedat.u-aizu.ac.jp/"), //testcase用
         };
 
         public async Task<string> LoginAsync(string id, string pass)
@@ -55,17 +87,17 @@ namespace Questions251616
             var content = new StringContent(json, Encoding.UTF8, "application/json");
             var response = await client.PostAsync("session", content);
             Debug.WriteLine($"content ->> {content}");
-            
+
             if (response.Headers.Contains("Set-Cookie"))
             {
                 var cookie = response.Headers.GetValues("Set-Cookie").First();
                 Console.WriteLine($"cookie --> {cookie}\n");
             }
-            
+
             return await response.Content.ReadAsStringAsync();
         }
-
-        public async Task<string> SubmissionAsync(string problem_id, string language,string code)
+        
+        public async Task<string> SubmissionAsync(string problem_id, string language, string code)
         {
             var json = $@"{{""problemId"":""{problem_id}"",""language"":""{language}"",""sourceCode"":""{code}""}}";
             //var json = "{\"problemId\":\"ITP1_1_A\",\"language\":\"C++\",\"sourceCode\":\"#include <iostream>\\nusing namespace std;\\nint main(void){\\n    cout << \"Hello World\" << endl;\\n}\"}";
@@ -81,12 +113,16 @@ namespace Questions251616
 
         public Task<string> SelfAsync() => GetAsync("self");
         public Task<string> JudgeAsync(int judge_id) => GetAsync("/verdicts/4252705");
+
+        
+        
+        public Task<string> GetproblemAsync(string URI) => GetAsync(URI);
         public async Task<string> GetAsync(string uri)
         {
             var response = await client.GetAsync(uri);
-            
+
             return await response.Content.ReadAsStringAsync();
-            
+
         }
     }
 }
